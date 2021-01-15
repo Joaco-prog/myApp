57fc772f304d3965619faf50913d283d1780a4ee
Joaco-prog <Jmartinez@colegioinglesdepuebla.edu.mx>
2021-01-15 12:11:53 -0600

    Proyecto-cip

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
-export const environment = {
+export const environment = { 
   production: true
+};
+export const firebaseConfig = {
+  apiKey: "AIzaSyAUxHJwsqEG5MzS1giCFpBYBYkXArxlQXs",
+  authDomain: "proyecto-coolj.firebaseapp.com",
+  projectId: "proyecto-coolj",
+  storageBucket: "proyecto-coolj.appspot.com",
+  messagingSenderId: "763746836368",
+  appId: "1:763746836368:web:345689a4dddd44c7eadd15",
+  measurementId: "G-4BE6PRV6LH"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,15 @@
 export const environment = {
   production: false
 };
-
+export const firebaseConfig = {
+  apiKey: "AIzaSyAUxHJwsqEG5MzS1giCFpBYBYkXArxlQXs",
+  authDomain: "proyecto-coolj.firebaseapp.com",
+  projectId: "proyecto-coolj",
+  storageBucket: "proyecto-coolj.appspot.com",
+  messagingSenderId: "763746836368",
+  appId: "1:763746836368:web:345689a4dddd44c7eadd15",
+  measurementId: "G-4BE6PRV6LH"
+};
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
